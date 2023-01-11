784f7957df023d9599bf779fe42af05001c64167
dchoi294 <dchoi294@gmail.com>
2023-01-10 18:57:54 -0800

    lab17

---
+++ README.md
@@ -0,0 +1,17 @@
+# LAB - Class 17
+
+## Project: Web Scraper
+
+Author: Don Choi
+
+Scrape a Wikipedia page of your choosing and record which passages need citations.
+
+## Setup
+
+- pip install requests
+- pip install beautifulsoup4
+
+## Resources
+
+- [History of Mexico](https://en.wikipedia.org/wiki/History_of_Mexico)
+- [Beautiful Soup 4.9.0 documentation - Crummy](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
---
+++ web_scraper/scraper.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def soup(url):
+    page = requests.get(url)
+    soup_ = BeautifulSoup(page.content, 'html.parser')
+    return soup_
+
+
+def get_citations_needed_count(url):
+    soup_ = soup(url)
+
+    count = len(soup_.find_all("sup", class_="noprint Inline-Template Template-Fact"))
+
+    print("\x1B[3m" + "number of missing citations: " + "\x1B[0m" + str(count))
+
+
+def get_citations_needed_report(url):
+    soup_ = soup(url)
+    report = soup_.find_all("sup", class_="noprint Inline-Template Template-Fact")
+    string = ""
+    sections = ""
+    for x in report:
+        string += ("\x1B[3m" + "Section:\n" + "\x1B[0m" + x.find_previous("h3").get_text()
+                   + ":\n" + "\x1B[3m" + "Entry: \n" + "\x1B[0m" +  x.parent.get_text() + "\n")
+
+        sections += x.find_previous("h3").get_text() + "\n"
+    string = string.replace("[edit]:", "")
+    print(string)
+
+
+if __name__ == '__main__':
+    # url = input('> enter full URL of a wikipedia page including https:// ')
+    url = "https://en.wikipedia.org/wiki/History_of_Mexico"
+    get_citations_needed_count(url)
+    get_citations_needed_report(url)
